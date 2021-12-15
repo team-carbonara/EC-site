7b14c2adf301ae1f35fe7d4657b1e9caf8018fbc
EC2 Default User <ec2-user@ip-172-31-19-211.ec2.internal>
2021-12-15 05:24:11 +0000

    [Add] 【管理者側】商品登録・編集・一覧画面・機能の実装

---
+++ app/controllers/admin/products_controller.rb
@@ -0,0 +1,41 @@
+class Admin::ProductsController < ApplicationController
+   
+  def new
+    @genres = Genre.all
+  end
+
+  def index
+    @products = Product.all
+  end
+
+  def create
+    @product = Product.new(params_product)
+    if @product.save
+      redirect_to admin_product_path(@product)
+    else
+      render "new"
+    end
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def edit
+    @genres = Genre.all
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    @product.update(params_product)
+    redirect_to admin_product_path(@product)
+  end
+
+  private
+
+  def params_product
+    params.require(:product).permit(:id, :genre_id, :name, :description, :image, :price, :sale_status)
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  namespace :admin do
+    resources :products
+    resources :genres
+  end
 
   namespace :public do
     resource :customer, only: [:show, :edit] do
